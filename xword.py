42e63fd0deb78f6bc7c280040a36e26a0bf1510e
TimeApollo <arnjcksn0845@yahoo.com>
2018-11-09 13:50:43 -0500

    finished word look up.

--- xword.py
+++ xword.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Crossword Solver Program"""
+"""
+Crossword Solver Program
 
-__author__ = "???"
+Description: displays possible words to fit crossword puzzle location based on
+dictionary of words
 
-# YOUR HELPER FUNCTION GOES HERE
+Author: Aaron Jackson
+Github: TimeApollo
+"""
+
+__author__ = "TimeApollo"
+import re
+
+
+def word_checker(in_word, words):
+    """checks in_word against words list, to see if any match pattern"""
+    """returns list as well as printing all matches"""
+    regex = in_word.replace(' ', '\\w')
+    word_list = []
+    for word in words:
+        if re.match(r'{0}'.format(regex), word) and len(in_word) is len(word):
+            print(word)
+            word_list.append(word)
+    if not word_list:
+        print("Sorry, No matches!")
+    return word_list
 
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
 
+    # spaces represent unknown letters
     test_word = raw_input(
-        'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+        'Please enter a word to solve.\n\
+        Use spaces to signify unknown letters: '
+    ).lower()
 
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    # test_word needs to be letters and spaces for unknown letters
+    word_checker(test_word, words)
 
 
 if __name__ == '__main__':
